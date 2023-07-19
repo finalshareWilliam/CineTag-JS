166373c69a64d8ed40783b1868921aa919585ca6
William Souza <william.h.souza@hotmail.com>
2023-07-19 14:12:15 -0300

    v2.1
    Projeto Final.

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,13 +1,21 @@
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
-import videos from 'json/db.json';
 import styles from './Inicio.module.css'
+import { useEffect, useState } from "react";
 
 function Inicio() {
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/finalshareWilliam/Cinetag-API/videos')
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideos(dados)
+            })
+    }, [])
+
     return (
         <>
-
             <Banner imagem="home" />
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
